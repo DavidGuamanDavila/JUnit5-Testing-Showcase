b66b43a2d13272c23e6611b4f05ef5bdbcfc2470
Anton David Guaman Davila <adavidguamandavila@gmail.com>
2024-01-30 23:53:59 -0330

    Update Test01FirstMocks.java
    Added the first test that uses mocking

--- HappyHotelAppTests/Test01FirstMocks.java
+++ HappyHotelAppTests/Test01FirstMocks.java
@@ -1,9 +1,11 @@
 package com.mockitotutorial.happyhotel.booking;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+
+import java.time.LocalDate;
 
 import org.junit.jupiter.api.*;
-import static org.mockito.Mockito.*;
 
 class Test01FirstMocks {
 
@@ -15,12 +17,22 @@ class Test01FirstMocks {
 
 	@BeforeEach
 	void setup() {
-
 		this.paymentServiceMock = mock(PaymentService.class);
 		this.roomServiceMock = mock(RoomService.class);
 		this.bookingDAOMock = mock(BookingDAO.class);
 		this.mailSenderMock = mock(MailSender.class);
 		this.bookingService = new BookingService(paymentServiceMock, roomServiceMock, bookingDAOMock, mailSenderMock);
+	}
+	
+	@Test
+	void should_CalculateCorrectPrice_WhenCorrectInput() {
+			// given
+			BookingRequest bookingRequest = new BookingRequest("1", LocalDate.of(2020, 01, 01), LocalDate.of(2020, 01, 05), 2, false);
+			double expected = 4 * 2 * 50.0;
+			// when
+			double actual = bookingService.calculatePrice(bookingRequest);
+			// taken
+			assertEquals(expected, actual);
 	}
 
 
