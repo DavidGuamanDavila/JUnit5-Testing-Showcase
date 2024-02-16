8280fbb5aabedf361d0433d995ce2579b1ce5f2c
Anton David Guaman Davila <adavidguamandavila@gmail.com>
2024-02-16 09:43:48 -0330

    Update and rename Test14Answers.java to Test15Answers.java

--- HappyHotelAppTests/Test15Answers.java
+++ HappyHotelAppTests/Test15Answers.java
@@ -15,7 +15,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 //Stubbing is default when using ExtendWith
 @ExtendWith(MockitoExtension.class)
-class Test14Answers{
+class Test15Answers{
 
 	@InjectMocks
 	private BookingService bookingService;
